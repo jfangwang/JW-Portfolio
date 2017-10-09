230053b3647c5d80fe6acfd9472ee354a66debbc
jfangwang <18_jwang@s.wmrhsd.org>
2017-10-09 19:31:45 -0400

    JWportfolio.js

---
+++ JWportfolio.js
@@ -0,0 +1,8 @@
+document.write("This is the JWportfolio Javascript document! 2");
+  function validator(){
+    if(document.JWForm.thebox.checked)
+      alert("It is checked!");
+    else
+      alert("It is not checked");
+
+  }
